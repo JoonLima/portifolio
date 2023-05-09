30b6388351182e58efc7dd9308c92826079c372c
Jonathan Ventura <jonathanlventura3@gmail.com>
2023-05-09 20:40:38 -0300

    ajuste responsividade mobile

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 const menuBtn = document.querySelector('#icon-menu');
 const menu = document.querySelector('.menu');
+const menuMobile = document.querySelector('.menu-mobile')
 
 function showMenu() {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open')
-        menuBtn.classList.remove('bx-x')
-        menuBtn.classList.add('bx-menu')
     } else {
         menu.classList.add('open')
-        menuBtn.classList.remove('bx-menu')
-        menuBtn.classList.add('bx-x')
+    }
+}
+
+function showMenuMobile(){
+    if(menuMobile.classList.contains('mobile-open')){
+        menuMobile.classList.remove('mobile-open')
+    } else {
+        menuMobile.classList.add('mobile-open')
     }
 }
