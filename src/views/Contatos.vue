3b52ad8c665431ec563a6b8ef8019a0c6af3eca8
Jonathan Ventura <jonathanlventura3@gmail.com>
2024-06-05 09:44:43 -0300

    ajuste nos contatos

--- src/views/Contatos.vue
+++ src/views/Contatos.vue
@@ -1,27 +1,27 @@
 <template>
   <v-row class="container">
-    <v-col lg="3" sm="12" xs="12" class="icone">
+    <v-col lg="3" md="3" sm="12" xs="12" class="icone">
       <a href="" target="_blank"
         ><v-icon color="primary" class="ic">mdi-email-outline</v-icon></a
       >
       <span class="texo-icone">E-mail</span>
       <span class="texo-icone">jonathan.vpp@gmail.com</span>
     </v-col>
-    <v-col lg="3" sm="12" xs="12" class="icone">
+    <v-col lg="3" md="3" sm="12" xs="12" class="icone">
       <a href="https://github.com/JoonLima" target="_blank"
         ><v-icon color="primary" class="ic">mdi-github</v-icon></a
       >
       <span class="texo-icone">Github</span>
       <span class="texo-icone">@JoonLima</span>
     </v-col>
-    <v-col lg="3" sm="12" xs="12" class="icone">
+    <v-col lg="3" md="3" sm="12" xs="12" class="icone">
       <a href="https://www.linkedin.com/in/jonathanlventura/" target="_blank"
         ><v-icon color="primary" class="ic">mdi-linkedin</v-icon></a
       >
       <span class="texo-icone">Linkedin</span>
       <span class="texo-icone">@jonathanlventura</span>
     </v-col>
-    <v-col lg="3" sm="12" xs="12" class="icone">
+    <v-col lg="3" md="3" sm="12" xs="12" class="icone">
       <a
         href="https://api.whatsapp.com/send/?phone=5521971935139&text&type=phone_number&app_absent=0"
         target="_blank"
@@ -73,5 +73,8 @@ export default {};
 }
 
 @media (max-width: 855px) {
+  .icone {
+    min-width: 100%;
+  }
 }
 </style>
